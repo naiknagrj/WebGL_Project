a54f7e4865496fb317835d628ab379894f5d2982
naiknagrj <75028098+naiknagrj@users.noreply.github.com>
2021-04-27 20:02:56 +0530

    Touch Panel for full screen interaction
    added a UI panel on either side to interact with the mouse to rotate and vertically move the gameobject

---
+++ Assets/scripts/MyRotationTouchpad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MyRotationTouchpad : MonoBehaviour
+{
+    public Transform Rotatable;
+    public float RotationSpeed = 0.37f;
+    bool ismouseheld;
+    Vector2 currentMousePosition;
+    Vector2 mouseDeltaPosition;
+    Vector2 lastMousePosition;
+    bool istouchpadactive;
+    private void Start()
+    {
+        currentMousePosition = Input.mousePosition;
+        lastMousePosition = currentMousePosition;
+        mouseDeltaPosition = currentMousePosition - lastMousePosition;
+    }
+    void Update()
+    {
+        if (istouchpadactive)
+        {
+            currentMousePosition = Input.mousePosition;
+            mouseDeltaPosition = currentMousePosition - lastMousePosition;
+            Rotatable.transform.Rotate(0f, mouseDeltaPosition.x * RotationSpeed, 0f);
+            lastMousePosition = currentMousePosition;
+        }
+    }
+    public void ActivateTouchpad()
+    {
+        istouchpadactive = true;
+    }
+    public void DeactivateTouchpad()
+    {
+        istouchpadactive = false;
+    }
+}
---
+++ Assets/scripts/UpDownTP.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UpDownTP : MonoBehaviour
+{
+    
+    public float Speed = 0.2f;
+    bool ismouseheld;
+    Vector2 currentMousePosition;
+    Vector2 mouseDeltaPosition;
+    Vector2 lastMousePosition;
+    bool istouchpadactive;
+    private void Start()
+    {
+        currentMousePosition = Input.mousePosition;
+        lastMousePosition = currentMousePosition;
+        mouseDeltaPosition = currentMousePosition - lastMousePosition;
+    }
+    void Update()
+    {
+        if (istouchpadactive)
+        {
+            currentMousePosition = Input.mousePosition;
+            mouseDeltaPosition = currentMousePosition - lastMousePosition;
+            transform.Translate(0f, mouseDeltaPosition.y * Speed*Speed*Speed, 0f);
+            lastMousePosition = currentMousePosition;
+        }
+    }
+    public void ActivateTouchpad()
+    {
+        istouchpadactive = true;
+    }
+    public void DeactivateTouchpad()
+    {
+        istouchpadactive = false;
+    }
+}
