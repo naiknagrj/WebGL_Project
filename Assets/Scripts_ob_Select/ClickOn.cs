b13dae3d272a2b3fb9491d9d865a5772ecff3775
naiknagrj <75028098+naiknagrj@users.noreply.github.com>
2021-05-06 18:55:02 +0530

    added Highlight prototype

---
+++ Assets/Scripts_ob_Select/ClickOn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOn : MonoBehaviour
+{
+    [SerializeField]
+    private Material InitialMaterial;
+
+    [SerializeField]
+    private Material green;
+
+    [HideInInspector]
+    public bool currentlySelected = false;
+
+    private MeshRenderer myRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRend = GetComponent<MeshRenderer>();
+        ClickMe();
+    }
+
+    public void ClickMe()
+    {
+        if (currentlySelected == false)
+        {
+            myRend.material = InitialMaterial;
+        }
+        else
+        {
+            myRend.material = green;
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts_ob_Select/HideSelection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideSelection : MonoBehaviour
+{
+    public void hideIt(GameObject Obj)
+    {
+        Obj.SetActive(false);
+    }
+}
